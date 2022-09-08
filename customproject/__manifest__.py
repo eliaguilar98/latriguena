80ce0bb4be4c8ff68ef9bbf6b0177c8b2e41b975
Eliz <eli@seditec.mx>
2022-09-08 04:28:15 +0000

    Sales Report

--- customproject/__manifest__.py
+++ customproject/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','project', 'industry_fsm', 'sale.order',],
+    'depends': ['project', 'industry_fsm', 'sale',],
 
     # always loaded
     'data': [
