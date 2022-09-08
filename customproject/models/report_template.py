5f1e6493d8983a2200b4a9e3cb8a609352ec63c3
Elizabeth <eli@seditec.mx>
2022-09-08 02:37:20 +0000

    [New report]

---
+++ customproject/models/report_template.py
@@ -0,0 +1,8 @@
+# -*- coding:utf-8 -*-
+
+from odoo import models, fields, api
+
+class ReportTemplate(models.Model):
+    _inherit = 'sale.report'
+    
+    
---
+++ customproject/wizard/sale_wizard.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*- 
+
+from odoo import models, fields, api
+
+class SaleWizard(model.TransientModel):
+    _name = 'report.sale.wizard'
+    _description = 'Wizard: Quick Sale Orders for SalePersons'
+    
+    user_id = fields.Many2one(comodel_name='sale.report',
+                             string='Saleperson',
+                             required=True)
+    date_creation = fields.Date(string='Creation Date')
+    
+    
+    # def action_print_report(self):
+        
